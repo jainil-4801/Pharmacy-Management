ab81c64b5c04f784210271d6e8acce04fa823cea
coder-cracker <201851116@iiitvadodara.ac.in>
2020-05-18 11:40:06 +0530

    updated

--- user/models.py
+++ user/models.py
@@ -15,3 +15,22 @@ class Product(models.Model):
 	def __str__(self):
 		return self.product_name
 
+class Orders(models.Model):
+    order_id = models.AutoField(primary_key=True)
+    items_json = models.CharField(max_length=500)
+    name = models.CharField(max_length=90)
+    email = models.CharField(max_length=111)
+    address = models.CharField(max_length=1110)
+    city = models.CharField(max_length=110)
+    state = models.CharField(max_length=110)
+    zip_code = models.CharField(max_length=110)
+    phone = models.CharField(max_length=110,default="")
+
+class OrderUpdate(models.Model):
+    update_id = models.AutoField(primary_key=True)
+    order_id = models.IntegerField(default="")
+    update_desc = models.CharField(max_length=5000)
+    timestamp = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.update_desc[0:7]+"..."
--- user/views.py
+++ user/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Product
+from .models import Product,Orders,OrderUpdate
 from math import ceil
 import json 
 # Create your views here.
@@ -26,7 +26,46 @@ def catproducts(request,cat):
 	return render(request,"user/catproducts.html",params)
 
 def checkout(request):
-	return HttpResponse("At checkout")
+    thank = "false" 
+    if request.method =="POST":
+        items_json = request.POST.get('itemsJson','')
+        name=request.POST.get('name','')
+        email=request.POST.get('email','')
+        address=request.POST.get('address1','')+" "+request.POST.get('address2','')
+        city=request.POST.get('city','')
+        state=request.POST.get('state','')
+        zip_code=request.POST.get('zip_code','')
+        phone=request.POST.get('phone','')
+        order = Orders(items_json = items_json,name=name,email=email,address = address,city=city,
+                        zip_code=zip_code,state=state, phone=phone)
+        order.save()
+        update = OrderUpdate(order_id=order.order_id,update_desc="The order has been placed")
+        update.save()
+        thank = "true" 
+        id = order.order_id 
+        print("In Post")
+        return render(request,'user/checkout.html',{'thank':thank,'id':id})
+    return render(request,'user/checkout.html',{'thank':thank})
+
+def tracker(request):
+    if request.method=="POST":
+        OrderId=request.POST.get('OrderId','')
+        email=request.POST.get('email','')
+        try:
+            order = Orders.objects.filter(order_id=OrderId,email=email)
+            if len(order)>0:
+                update = OrderUpdate.objects.filter(order_id=OrderId)
+                updates = []
+                for item in update:
+                    updates.append({'text':item.update_desc,'time':item.timestamp})
+                    response = json.dumps([updates,order[0].items_json],default=str)
+                return HttpResponse(response) 
+            else:    
+                return HttpResponse('{}')
+        except Exception as e:
+            return HttpResponse('{}')
+    return render(request,'user/tracker.html')
+
 
 def search(request):
 	return HttpResponse("At search")
